c9554ef8c08d27aedde4685c471eea3f28b1a252
Nucletic <96342951+Nucletic@users.noreply.github.com>
2024-05-11 12:51:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
